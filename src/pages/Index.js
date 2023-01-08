494556fc005ae8158c1e0e9c517cee5823b586f7
Your Name <you@example.com>
2023-01-08 13:29:58 -0500

    update pages/Index to display list of cheeses.  Add to styles.scss max-width for images on index page

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -2,8 +2,24 @@
 //  Index Page
 //----------------------
 
+//----------------------
+//  import dependencies
+//----------------------
+import { Link, useLoaderData } from "react-router-dom" 
+
 function Index (props) {
-    return <h1>Index</h1>
+    //define list of cheeses from loader data
+    const cheeses = useLoaderData();
+
+    return cheeses.map(cheese => (
+        <div key={cheese._id} className="cheese">
+            <Link to={`/${cheese.id}`}>
+                <h1>{cheese.name}</h1>
+            </Link>
+            <h3>Country of Origin: {cheese.countryOfOrigin}</h3>
+            <img src={cheese.image} alt={cheese.name} className="index-img-sz"/>
+        </div>
+    ))
 }
 
 //----------------------
