1718070111c4ed2509aa7876703080d94181642a
Your Name <you@example.com>
2023-01-08 13:41:23 -0500

    add create cheese form to pages/Index.js

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -5,21 +5,34 @@
 //----------------------
 //  import dependencies
 //----------------------
-import { Link, useLoaderData } from "react-router-dom" 
+import { Form, Link, useLoaderData } from "react-router-dom" 
 
 function Index (props) {
     //define list of cheeses from loader data
     const cheeses = useLoaderData();
 
-    return cheeses.map(cheese => (
-        <div key={cheese._id} className="cheese">
-            <Link to={`/${cheese.id}`}>
-                <h1>{cheese.name}</h1>
-            </Link>
-            <h3>Country of Origin: {cheese.countryOfOrigin}</h3>
-            <img src={cheese.image} alt={cheese.name} className="index-img-sz"/>
+    return (
+        <div>
+            <h2>Create a Cheese</h2>
+            <Form action="/create" method="post">
+                <input type="input" name="name" placeholder="cheese name" />
+                <input type="input" name="countryOfOrigin" placeholder="country of origin" />
+                <input type="input" name="image" placeholder="picture of cheese" />
+                <input type="submit" value="create cheese" />
+            </Form>
+
+            <h2>Cheeses</h2>
+                {cheeses.map(cheese => (
+                <div key={cheese._id} className="cheese">
+                    <Link to={`/${cheese.id}`}>
+                        <h1>{cheese.name}</h1>
+                    </Link>
+                    <h3>Country of Origin: {cheese.countryOfOrigin}</h3>
+                    <img src={cheese.image} alt={cheese.name} className="index-img-sz"/>
+                </div>
+                ))}
         </div>
-    ))
+    )
 }
 
 //----------------------
