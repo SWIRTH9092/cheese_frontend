9b2a348d8c7823ff9efdeabedc965ed31e03a9ef
Your Name <you@example.com>
2023-01-08 11:53:33 -0500

    update header to add navigation bar.  add import dependency of Link.

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,20 @@
 //  Header Component
 //----------------------
 
+
+//----------------------
+//  import Dependencies
+//----------------------
+import { Link } from "react-router-dom"
+
 function Header(props) {
-    return <h1>Header</h1>
+    return (
+        <nav className="nav">
+            <Link to="/">
+                <div>Cheese App</div>
+            </Link>
+        </nav>
+    )
 }
 
 //----------------------
