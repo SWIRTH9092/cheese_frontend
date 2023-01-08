dfcb8274de3b26cfb205bd588d12b279c9ed568e
Your Name <you@example.com>
2023-01-08 17:34:46 -0500

    add form to pages/Show.js

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -5,17 +5,26 @@
 //----------------------
 // Import dependencies
 //----------------------
-import {useLoaderData} from "react-router-dom"
+import { useLoaderData, Form } from "react-router-dom"
 
 function Show (props) {
 
     const cheese = useLoaderData()
 
     return (
-        <div className="cheese">
+        <div className="cheese">    
             <h1>{cheese.name}</h1>
             <h2>Country of Origin: {cheese.countryOfOrigin}</h2>
             <img src={cheese.image} alt={cheese.name} className="show-img-sz" />
+
+            <h2>Update {cheese.name}</h2>
+            <Form action ={`/update/${cheese._id}`} method="post">
+                <input type="input" name="name" placeholder="cheese name" />
+                <input type="input" name="cheese.countryOfOrigin" placeholder="cheese.countryOfOrigin" />
+                <input type="input" name="image" placeholder="link for cheese image" />
+                <input type="submit" value={`update ${cheese.name}`} />
+            </Form>
+
         </div>
     )
 }
