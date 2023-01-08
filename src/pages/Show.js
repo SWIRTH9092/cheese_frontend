3212786d07a7a67e053534dc48516ef054d1f240
Your Name <you@example.com>
2023-01-08 10:58:04 -0500

    add base definition to show page

---
+++ src/pages/Show.js
@@ -0,0 +1,12 @@
+//----------------------
+//  Show Page
+//----------------------
+
+function Show (props) {
+    return <h1>Show</h1>
+}
+
+//----------------------
+//  Export Index Page
+//----------------------
+export default Show;
