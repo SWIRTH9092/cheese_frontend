fbc3043abdc35992c662138f616cf7d6340b478e
Your Name <you@example.com>
2023-01-08 17:41:28 -0500

    add updateCheese to actions.js

--- src/actions.js
+++ src/actions.js
@@ -35,3 +35,25 @@ export const createAction = async ({ request }) => {
     // redirect to index
     return redirect("/")
 }
+
+
+export const updateAction = async ({request, params}) => {
+    //get data from form
+    const formData = await request.formData()
+    //set update cheese to match schema
+    const updatedCheese = {
+        name: formData.get("name"),
+        countryOfOrigin: formData.get("countryOfOrigin"),
+        image: formData.get("image")
+    }
+    // Send updated cheese to our API
+    await fetch(URL + "/cheese/" + params.id, {
+        method: "put",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(updatedCheese)
+    })
+        // redirect to index
+        return redirect("/")
+}
