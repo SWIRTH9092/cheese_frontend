1adc11e58cf463b595c0e2561db1ac3ef1e1c156
Your Name <you@example.com>
2023-01-08 11:11:05 -0500

    update router for all routes.  for index and show, add element details

--- src/router.js
+++ src/router.js
@@ -7,6 +7,8 @@ import {
     Route,
 }  from "react-router-dom"
 import App from "./App"
+import Index from "./pages/Index"
+import Show from "./pages/Show"
 
 //---------------------------------------
 //   Router Paths
@@ -14,7 +16,11 @@ import App from "./App"
 const router = createBrowserRouter(
         createRoutesFromElements(
             <Route path="/" element={<App />}>
-            
+                <Route path="" element={<Index />} />
+                <Route path=":id" element={<Show />} />
+                <Route path="create" />
+                <Route path="update/:id" />
+                <Route path="delete/:id" />
             </Route>)
 )
 
